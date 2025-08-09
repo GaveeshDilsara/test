cce44807d246c228241dbac971e1562f6da44ebd
Gaveesh <gaveeshrcg@gmail.com>
2025-08-09 21:24:40 +0530

    addded 4 lines

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 
 //import { Button } from "@react-navigation/elements";
 import React from "react";
-import { Button, View } from "react-native";
+import { Button, Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -9,6 +9,11 @@ export default function Index() {
       <View style={{backgroundColor:'#ffffff',padding:20,borderRadius:10}}>
             <Button color={'yellow'} title="Click Me" onPress={() => console.log('Button Presssed')}/>
       </View>
+
+      <Text style={{color:'#ffffff',marginTop:20}}>Hello, this is a simple app!</Text>
+      <Text style={{color:'#ffffff'}}>This is a React Native application.</Text>
+      <Text style={{color:'#ffffff'}}>Enjoy coding!</Text>
+      <Text style={{color:'#ffffff'}}>Have a great day!</Text>
 
     </View>
   );
